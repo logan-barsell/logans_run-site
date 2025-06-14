7b87029216ebbe12ae409d1185197fd9a4d4632f
Logan Barsell <loganjbars@gmail.com>
2025-06-13 17:06:41 -0700

    CSS updates

--- client/src/pagesAdmin/Music/MusicEdit.js
+++ client/src/pagesAdmin/Music/MusicEdit.js
@@ -18,63 +18,91 @@ const MusicEdit = ({ fetchPlayers, players }) => {
     const path = new URL(player.spotifyLink).pathname;
     const theme = player.bgColor ? player.bgColor : '';
     const embedLink = `https://open.spotify.com/embed${path}?utm_source=generator${theme}`;
-    const updatedPlayer = {...player, embedLink, bgColor: theme};
+    const updatedPlayer = { ...player, embedLink, bgColor: theme };
     console.log(updatedPlayer);
-    axios.post('/api/updatePlayer', updatedPlayer)
+    axios
+      .post('/api/updatePlayer', updatedPlayer)
       .then(res => fetchPlayers())
       .catch(err => console.log(err));
   };
 
   const deletePlayer = id => {
-    axios.get(`/api/deletePlayer/${id}`)
+    axios
+      .get(`/api/deletePlayer/${id}`)
       .then(res => fetchPlayers())
       .catch(err => console.log(err));
-  }
+  };
 
   const renderPlayers = players.map(player => {
     const dateString = new Date(player.date).toLocaleDateString();
-    const color = !player.bgColor ? '#e81d10' : '#282828'
+    const color = !player.bgColor ? '#e81d10' : '#282828';
     return (
-      <div key={player._id} className="list-group-item list-group-item-action" aria-current="true">
-        <div className="d-flex w-100 justify-content-end">
-          <small><span>Released </span>{dateString}</small>
+      <div
+        key={player._id}
+        className='list-group-item list-group-item-action'
+        aria-current='true'
+      >
+        <div className='d-flex w-100 justify-content-end'>
+          <small>
+            <span>Released </span>
+            {dateString}
+          </small>
         </div>
-        <div className="d-flex w-100 justify-content-center">
-          <h5 className="mb-2">{player.title}</h5>
+        <div className='d-flex w-100 justify-content-center'>
+          <h5 className='mb-2'>{player.title}</h5>
         </div>
-        <p className="mb-3 d-flex justify-content-center">
-          <a href={player.spotifyLink} target="_blank" rel="no-referrer" className="btn btn-sm btn-light">Link to Spotify</a>
+        <p className='mb-3 d-flex justify-content-center'>
+          <a
+            href={player.spotifyLink}
+            target='_blank'
+            rel='no-referrer'
+            className='btn btn-sm btn-light'
+          >
+            Link to Spotify
+          </a>
         </p>
-        <small className="d-flex justify-content-center align-items-center mb-2">
-          <span>Theme </span> &nbsp; 
-          <div className="theme" style={{ 'backgroundColor': `${color}`}}>
-          </div>
+        <small className='d-flex justify-content-center align-items-center mb-2'>
+          <span>Theme </span> &nbsp;
+          <div
+            className='theme'
+            style={{ backgroundColor: `${color}` }}
+          ></div>
         </small>
-        <hr/>
-        <div className="buttons d-flex justify-content-center">
-          <EditPlayer player={player} editFields={editPlayerFields} onEdit={editPlayer}/>
-          <DeletePlayer player={player}  onDelete={deletePlayer}/>
+        <hr />
+        <div className='buttons d-flex justify-content-center'>
+          <EditPlayer
+            player={player}
+            editFields={editPlayerFields}
+            onEdit={editPlayer}
+          />
+          <DeletePlayer
+            player={player}
+            onDelete={deletePlayer}
+          />
         </div>
       </div>
     );
-  })
+  });
 
   return (
     <>
-      <div id="music-edit" className="row">
+      <div
+        id='music-edit'
+        className='row'
+      >
         <h3>Edit Music</h3>
-        <hr/>
-        <AddPlayer/>
-        <div id="currentPlayers" className="list-group">
-          {
-            renderPlayers
-          }
+        <hr />
+        <AddPlayer />
+        <div
+          id='currentPlayers'
+          className='list-group'
+        >
+          {renderPlayers}
         </div>
       </div>
-
     </>
   );
-}
+};
 
 function mapStateToProps({ music }) {
   return { players: music };
--- client/src/pagesAdmin/index.js
+++ client/src/pagesAdmin/index.js
@@ -4,7 +4,6 @@ import '../plugins/loading-bar.css';
 import React, { useState, createContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TopNavEdit from '../components/Navbar/NavBarEdit';
-// import BottomNav from '../Navbar/BottomNav';
 import HomeEdit from './Home/HomeEdit';
 import MusicEdit from './Music/MusicEdit';
 import MerchEdit from './Merch/MerchEdit';
@@ -13,19 +12,16 @@ import BioEdit from './Bio/BioEdit';
 import ContactEdit from './Contact/ContactEdit';
 import history from '../history';
 
-
-
 export const ActiveContext = createContext();
 
 const EditPages = () => {
-
   const routes = [
     { name: 'Home', value: '/home' },
     { name: 'Music', value: '/music' },
     { name: 'Store', value: '/merch' },
     { name: 'Media', value: '/media' },
     { name: 'Bio', value: '/aboutus' },
-    { name: 'Contact', value: '/contact' }
+    { name: 'Contact', value: '/contact' },
   ];
 
   const currentUrl = window.location.pathname;
@@ -34,30 +30,55 @@ const EditPages = () => {
   for (let i = 0; i < routes.length - 1; i++) {
     if (routes[i].value === currentUrl) {
       initialState = i;
-    };
-  };
+    }
+  }
 
   const [activeIndex, setActiveIndex] = useState(initialState);
   const [toggle, setToggle] = useState(false);
 
-
   return (
     <>
       <Router history={history}>
-        <ActiveContext.Provider value={{ activeIndex, setActiveIndex, toggle, setToggle }}>
+        <ActiveContext.Provider
+          value={{ activeIndex, setActiveIndex, toggle, setToggle }}
+        >
           <TopNavEdit routes={routes} />
           <Routes>
-            <Route path="/home" exact element={<HomeEdit />} />
-            <Route path="/music" exact element={<MusicEdit />} />
-            <Route path="/merch" exact element={<MerchEdit />} />
-            <Route path="/media" exact element={<MediaEdit />} />
-            <Route path="/aboutus" exact element={<BioEdit />} />
-            <Route path="/contact" exact element={<ContactEdit />} />
+            <Route
+              path='/home'
+              exact
+              element={<HomeEdit />}
+            />
+            <Route
+              path='/music'
+              exact
+              element={<MusicEdit />}
+            />
+            <Route
+              path='/merch'
+              exact
+              element={<MerchEdit />}
+            />
+            <Route
+              path='/media'
+              exact
+              element={<MediaEdit />}
+            />
+            <Route
+              path='/aboutus'
+              exact
+              element={<BioEdit />}
+            />
+            <Route
+              path='/contact'
+              exact
+              element={<ContactEdit />}
+            />
           </Routes>
         </ActiveContext.Provider>
       </Router>
     </>
   );
-}
+};
 
 export default EditPages;
