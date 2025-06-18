bfa2db5ad57ab535fdb8f94ac915c1c068cc0f14
Logan Barsell <loganjbars@gmail.com>
2025-06-18 10:45:12 -0700

    pace loading updates

--- client/src/index.js
+++ client/src/index.js
@@ -4,21 +4,23 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 // for authentication
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "./authConfig";
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
-
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+    <PersistGate
+      loading={null}
+      persistor={persistor}
+    >
       <MsalProvider instance={msalInstance}>
         <App />
       </MsalProvider>
     </PersistGate>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
