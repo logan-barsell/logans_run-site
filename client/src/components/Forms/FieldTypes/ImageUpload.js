56730c1309f49febbd78f86c6dc1a355146aeb02
Logan Barsell <loganjbars@gmail.com>
2025-06-18 12:50:03 -0700

    ui updates

--- client/src/components/Forms/FieldTypes/ImageUpload.js
+++ client/src/components/Forms/FieldTypes/ImageUpload.js
@@ -8,23 +8,36 @@ const ImageUpload = ({ name, initialValue }) => {
   const isRequired = initialValue ? false : true;
 
   return (
-    <div className="form-group">
-      <Field name={name} validate={isRequired && required}>
+    <div className='form-group'>
+      <Field
+        name={name}
+        validate={isRequired && required}
+      >
         {({ input: { value, onChange, ...input } }) => (
           <>
-            <span className="selectedFile">{value ? value[0]?.name : 'No File Selected'}</span>
-            <div className="fileUpload btn btn-danger">
+            <span className='selectedFile'>
+              {value ? value[0]?.name : 'No File Selected'}
+            </span>
+            <div className='fileUpload btn btn-secondary'>
               <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-camera-fill" viewBox="0 0 16 16">
-                  <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-                  <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z" />
-                </svg> Upload Image
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='25'
+                  height='25'
+                  fill='currentColor'
+                  className='bi bi-camera-fill'
+                  viewBox='0 0 16 16'
+                >
+                  <path d='M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z' />
+                  <path d='M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z' />
+                </svg>{' '}
+                Upload Image
               </span>
               <input
-                className="form-control upload"
+                className='form-control upload'
                 {...input}
-                type="file"
-                accept=".png, .jpg, .jpeg"
+                type='file'
+                accept='.png, .jpg, .jpeg'
                 onChange={({ target }) => onChange(target?.files)}
                 required={isRequired}
               />
@@ -33,7 +46,7 @@ const ImageUpload = ({ name, initialValue }) => {
         )}
       </Field>
     </div>
-  )
+  );
 };
 
 export default ImageUpload;
