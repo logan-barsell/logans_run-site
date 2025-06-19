7f232a9043046186727c11fa14c12873168b30c3
Logan Barsell <loganjbars@gmail.com>
2025-06-18 18:17:24 -0700

    update css

--- client/src/pages/Media/Video.js
+++ client/src/pages/Media/Video.js
@@ -1,54 +1,64 @@
 import React from 'react';
 
 const Video = ({ video }) => {
-    const months = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-    ];
-    
-    const daySuffix = day => {
-        const lastDigit = Number(day.toString()[day.toString().length - 1]);
-        if(lastDigit === 1 && lastDigit !== 11) {
-            return 'st';
-        }
-        if(lastDigit === 2 && lastDigit !== 12) {
-            return 'nd';
-        }
-        if(lastDigit === 3 && lastDigit !== 13) {
-            return 'rd';
-        }
-        return 'th';
-    };
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
 
-    const date = new Date(video.date);
-    const month = months[date.getMonth()].replace("0", "O");
-    const day = String(date.getDate()).replace("0", "O");
-    const year = String(date.getFullYear()).replace("0", "O");
-    const dateFormatted = `${month} ${day}${daySuffix(day)}, ${year}`
+  const daySuffix = day => {
+    const lastDigit = Number(day.toString()[day.toString().length - 1]);
+    if (lastDigit === 1 && lastDigit !== 11) {
+      return 'st';
+    }
+    if (lastDigit === 2 && lastDigit !== 12) {
+      return 'nd';
+    }
+    if (lastDigit === 3 && lastDigit !== 13) {
+      return 'rd';
+    }
+    return 'th';
+  };
 
-    return (
-            <div className="blog-post">
-                <div className="title">{video.title}</div>
-                <hr />
-                <div className="date">{dateFormatted}</div>
-                <div className="video embed-responsive embed-responsive-16by9">
-                    <iframe title={`${video.id}`} className="embed-responsive-item" src={video.embedLink} allowFullScreen></iframe>
-                </div>
-                <div className="yt-api-cont">
-                    <div className="g-ytsubscribe" data-channelid="UC_jExvqWhRlM-gBt9iEsLxA" data-layout="default" data-count="default"></div>
-                </div>
-            </div>
-    );
+  const date = new Date(video.date);
+  const month = months[date.getMonth()];
+  const day = String(date.getDate());
+  const year = String(date.getFullYear());
+  const dateFormatted = `${month} ${day}${daySuffix(day)}, ${year}`;
+
+  return (
+    <div className='blog-post'>
+      <div className='title'>{video.title}</div>
+      <hr />
+      <div className='date'>{dateFormatted}</div>
+      <div className='video embed-responsive embed-responsive-16by9'>
+        <iframe
+          title={`${video.id}`}
+          className='embed-responsive-item'
+          src={video.embedLink}
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className='yt-api-cont'>
+        <div
+          className='g-ytsubscribe'
+          data-channelid='UC_jExvqWhRlM-gBt9iEsLxA'
+          data-layout='default'
+          data-count='default'
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Video;
