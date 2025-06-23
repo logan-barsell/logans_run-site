c19ee8503de1f7fbd54cdb4366381fc97f49fa9b
Logan Barsell <loganjbars@gmail.com>
2025-06-23 14:23:44 -0700

    fix products + cart ui

--- client/src/pages/Merch/Products.js
+++ client/src/pages/Merch/Products.js
@@ -35,7 +35,7 @@ const Products = ({ fetchProducts, products }) => {
                 <div className='card-body'>
                   <h5 className='card-title product-name'>{product.name}</h5>
 
-                  <div className='price'>
+                  <div className='price secondary-font'>
                     $<span>{price.unit_amount / 100}</span>
                   </div>
                   <div className='d-grid gap-2'>
@@ -46,7 +46,17 @@ const Products = ({ fetchProducts, products }) => {
                       data-bs-target='#shoppingCart'
                       aria-controls='shoppingCart'
                     >
-                      Add to Car t
+                      Add to Car t{' '}
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='20'
+                        height='20'
+                        fill='currentColor'
+                        className='bi bi-plus-square-fill'
+                        viewBox='0 0 16 16'
+                      >
+                        <path d='M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z' />
+                      </svg>
                     </button>
                     {/* } */}
                   </div>
